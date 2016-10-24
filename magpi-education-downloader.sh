6d2ab295319f4b59d17f3a52b47a871cd9b35b63
Joergi <joerg.liedl@rocket-design.de>
2016-10-24 22:29:55 +0200

    Create magpi-education-downloader.sh
    adds education issues

---
+++ magpi-education-downloader.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# [Author] joergi77 - https://github.com/joergi77
+#          downloader for all MagPi issues
+#          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
+#          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
+#          this script is under GNU GENERAL PUBLIC LICENSE 
+# ------------------------------------------------------------------
+
+VERSION=0.1.0
+USAGE="Usage: sh magpi-education-downloader.sh"
+
+
+if [ ! -d "education_issues" ]; then
+ mkdir education_issues
+fi
+
+
+i=1
+issues=2
+
+
+while [ $i -le $issues ]
+do
+	if [ "$i" -lt 10 ]; then
+		wget https://www.raspberrypi.org/magpi-issues/MagPi-EduEdition0$i.pdf -P education_issues/
+		
+	else
+		wget https://www.raspberrypi.org/magpi-issues/MagPi-EduEdition$i.pdf -P education_issues/
+	fi
+	i=$(( i+1 ))
+done
