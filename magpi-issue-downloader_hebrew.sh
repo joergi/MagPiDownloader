ab0db97feca0eba5e095a4f5b5d3be9f22755792
joergi <j.liedl@dialogworks.de>
2017-01-03 19:40:06 +0100

    translated version

---
+++ magpi-issue-downloader_hebrew.sh
@@ -0,0 +1,36 @@
+# hebrew 
+
+#!/bin/sh
+
+# ------------------------------------------------------------------
+# [Author] joergi77 - https://github.com/joergi77
+#          downloader for all MagPi issues
+#          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
+#          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
+#          this script is under GNU GENERAL PUBLIC LICENSE 
+# ------------------------------------------------------------------
+
+VERSION=0.1.0
+USAGE="Usage: sh magpi-issue-downloader_hebrew.sh"
+
+
+if [ ! -d "hebrew_issues" ]; then
+ mkdir hebrew_issues
+fi
+
+
+i=1
+issues=2
+
+
+while [ $i -le $issues ]
+do
+	if [ "$i" -lt 10 ]; then
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Hebrew_0$i.pdf -P spanish_issues/
+		
+	else
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Hebrew_$i.pdf -P spanish_issues/
+	fi
+	i=$(( i+1 ))
+done
+
---
+++ magpi-issue-downloader_italian.sh
@@ -0,0 +1,38 @@
+# italian
+
+
+#!/bin/sh
+
+# ------------------------------------------------------------------
+# [Author] joergi77 - https://github.com/joergi77
+#          downloader for all MagPi issues
+#          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
+#          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
+#          this script is under GNU GENERAL PUBLIC LICENSE 
+# ------------------------------------------------------------------
+
+VERSION=0.1.0
+USAGE="Usage: sh magpi-issue-downloader_italian.sh"
+
+
+if [ ! -d "italian_issues" ]; then
+ mkdir italian_issues
+fi
+
+
+i=1
+issues=2
+
+
+while [ $i -le $issues ]
+do
+	if [ "$i" -lt 10 ]; then
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Italian_0$i.pdf -P italian_issues/
+		
+	else
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Italian_$i.pdf -P italian_issues/
+	fi
+	i=$(( i+1 ))
+done
+
+
