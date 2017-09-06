61cbddc94e4bbdc43b660a8a5ebf79c9b79cede9
Joergi <joergi@users.noreply.github.com>
2017-09-07 00:02:37 +0200

    Update magpi-special-issue-downloader.sh
    changes from sh to bash

--- magpi-special-issue-downloader.sh
+++ magpi-special-issue-downloader.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # ------------------------------------------------------------------
-# [Author] joergi77 - https://github.com/joergi77
+# [Author] joergi - https://github.com/joergi/MagPiDownloader
 #          downloader for all MagPi issues
 #          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
 #          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
@@ -44,4 +44,3 @@ wget https://www.raspberrypi.org/magpi-issues/Essentials_C_v1.pdf -P special_iss
 
 # Camera Essentials - The Camera Module Guide https://www.raspberrypi.org/magpi/issues/essentials_camera_v1/
 wget https://www.raspberrypi.org/magpi-issues/Essentials_Camera_v1.pdf - P special_issues/
-
