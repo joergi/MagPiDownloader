7912fdbbb7ee5a6662e8adc98d5e14b0b45e5fc4
Joergi <joergi@users.noreply.github.com>
2017-09-07 00:01:21 +0200

    Update magpi-issue-downloader_italian.sh
    changes from sh to bash

--- magpi-issue-downloader_italian.sh
+++ magpi-issue-downloader_italian.sh
@@ -1,10 +1,9 @@
 # italian
 
-
-#!/bin/sh
+#!/bin/bash
 
 # ------------------------------------------------------------------
-# [Author] joergi77 - https://github.com/joergi77
+# [Author] joergi - https://github.com/joergi/MagPiDownloader
 #          downloader for all MagPi issues
 #          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
 #          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
@@ -14,25 +13,19 @@
 VERSION=0.1.0
 USAGE="Usage: sh magpi-issue-downloader_italian.sh"
 
-
 if [ ! -d "italian_issues" ]; then
  mkdir italian_issues
 fi
 
-
 i=1
 issues=3
-
 
 while [ $i -le $issues ]
 do
 	if [ "$i" -lt 10 ]; then
-		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Italian_0$i.pdf -P italian_issues/
-		
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Italian_0$i.pdf -P italian_issues/		
 	else
 		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Italian_$i.pdf -P italian_issues/
 	fi
 	i=$(( i+1 ))
 done
-
-
