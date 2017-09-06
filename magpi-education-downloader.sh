5c98776120013cd612b6f79e644683621a6fa65f
Joergi <joergi@users.noreply.github.com>
2017-09-06 23:59:10 +0200

    Update magpi-education-downloader.sh

--- magpi-education-downloader.sh
+++ magpi-education-downloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-# [Author] joergi77 - https://github.com/joergi77
+# [Author] joergi - https://github.com/joergi/MagPiDownloader
 #          downloader for all MagPi issues
 #          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
 #          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
