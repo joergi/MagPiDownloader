0ee41cb23293dffe46fc534ec68ca3e599410889
Rubem Mota <rubemmota89@gmail.com>
2017-10-05 22:33:05 +0100

    ReAdded bash shell scripts

---
+++ magpi-issue-downloader_french.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# french version
+# ------------------------------------------------------------------
+# [Author] joergi - https://github.com/joergi/MagPiDownloader
+#          downloader for all MagPi issues
+#          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
+#          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
+#          this script is under GNU GENERAL PUBLIC LICENSE
+# ------------------------------------------------------------------
+
+
+# VERSION=0.1.0
+# USAGE="Usage: sh magpi-issue-downloader_french.sh"
+
+
+if [ ! -d "french_issues" ]; then
+ mkdir french_issues
+fi
+
+
+i=1
+issues=3
+
+
+while [ $i -le $issues ]
+do
+	if [ "$i" -lt 10 ]; then
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_French_0$i.pdf -P french_issues/
+
+	else
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_French_$i.pdf -P french_issues/
+	fi
+	i=$(( i+1 ))
+done
---
+++ magpi-issue-downloader_spanish.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# spanish version
+# ------------------------------------------------------------------
+# [Author] joergi - https://github.com/joergi/MagPiDownloader
+#          downloader for all MagPi issues
+#          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
+#          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
+#          this script is under GNU GENERAL PUBLIC LICENSE
+# ------------------------------------------------------------------
+
+# VERSION=0.1.0
+# USAGE="Usage: sh magpi-issue-downloader_spanish.sh"
+
+if [ ! -d "spanish_issues" ]; then
+ mkdir spanish_issues
+fi
+
+i=1
+issues=3
+
+while [ $i -le $issues ]
+do
+	if [ "$i" -lt 10 ]; then
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Spanish_0$i.pdf -P spanish_issues/
+	else
+		wget https://www.raspberrypi.org/magpi-issues/MagPi_Mini_Spanish_$i.pdf -P spanish_issues/
+	fi
+	i=$(( i+1 ))
+done
