754d1f42d80437cfb4323872d57e82d75a842b7b
joergi <1439809+joergi@users.noreply.github.com>
2020-07-13 00:25:04 +0200

    fixing linter issue: https://github.com/koalaman/shellcheck/wiki/SC2086

--- linux_mac/magpi-issue-downloader.sh
+++ linux_mac/magpi-issue-downloader.sh
@@ -23,7 +23,7 @@ downloadUrl="https://magpi.raspberrypi.org/issues/%02d/pdf"
 file="$BASEDIR/issues.txt";
 recentIssue=$(cat "$file");
 
-source <(curl -s https://raw.githubusercontent.com/joergi/downloader/master/linux_mac/downloader.sh) $downloadUrl $recentIssue "$@"
+source <(curl -s https://raw.githubusercontent.com/joergi/downloader/master/linux_mac/downloader.sh) "$downloadUrl" "$recentIssue" "$@"
 
 exit 0
 
