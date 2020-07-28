a8c0954aca9ff70f5b1742f007bd2403b0bd7afa
joergi <1439809+joergi@users.noreply.github.com>
2020-07-29 01:12:47 +0200

    use downloader 0.3.0 for special-issues and normal ones

--- linux_mac/magpi-special-issue-downloader.sh
+++ linux_mac/magpi-special-issue-downloader.sh
@@ -17,49 +17,11 @@ if [ ! -d "$OUTDIR" ]; then
  mkdir "$OUTDIR"
 fi
 
-# Essential Games v1
-wget -N https://www.raspberrypi.org/magpi-issues/Essentials_Games_v1.pdf -P "$OUTDIR"
 
-# Project Book - v1 https://www.raspberrypi.org/magpi/issues/projects-1/
-wget -N https://raspberrypi.org/magpi-issues/Projects_Book_v1.pdf -P "$OUTDIR"
+file=$BASEDIR"/sources-for-download/special-issues.txt"
+while IFS= read -r line
+do
+	bash <(curl https://raw.githubusercontent.com/joergi/downloader/special_edition_changes/linux_mac/downloader.sh) $line $OUTDIR
+done < "$file"
 
-# Project Book - v2 https://www.raspberrypi.org/magpi/issues/projects-2/
-wget -N https://www.raspberrypi.org/magpi-issues/Projects_Book_v2.pdf -P "$OUTDIR"
-
-# Project Book - v3 https://www.raspberrypi.org/magpi/issues/projects-3/
-wget -N https://www.raspberrypi.org/magpi-issues/Projects_Book_v3.pdf -P "$OUTDIR"
-
-# Bash Essentials v1 - https://www.raspberrypi.org/magpi/issues/essentials-bash-vol1/
-wget -N https://raspberrypi.org/magpi-issues/Essentials_Bash_v1.pdf -P "$OUTDIR"
-
-# Essentials Sense Hat v1 - https://www.raspberrypi.org/magpi/issues/essentials-sense-hat-v1/
-wget -N https://www.raspberrypi.org/magpi-issues/Essentials_SenseHAT_v1.pdf -P "$OUTDIR"
-
-# Sonic Pi Essentials - Code Music with Sonic Pi - https://www.raspberrypi.org/magpi/issues/essentials-sonic-pi-v1/
-wget -N https://raspberrypi.org/magpi-issues/Essentials_Sonic_Pi-v1.pdf -P "$OUTDIR"
-
-# Scratch Essentials - Learn to code with scratch - https://www.raspberrypi.org/magpi/issues/essentials-scratch-v1/
-wget -N https://www.raspberrypi.org/magpi-issues/Essentials_Scratch_v1.pdf -P "$OUTDIR"
-
-# Minecraft Essentials - Hacking and Making in Minecraft - https://www.raspberrypi.org/magpi/issues/essentials-minecraft-v1/
-wget -N https://raspberrypi.org/magpi-issues/Essentials_Minecraft_v1.pdf -P "$OUTDIR"
-
-# GPIO Zero Essentials - Simple Electronics with GPIO Zero
-wget -N https://raspberrypi.org/magpi-issues/Essentials_GPIOZero_v1.pdf -P "$OUTDIR"
-
-# C Essentials - Learn to code with C - https://www.raspberrypi.org/magpi/issues/essentials-c-v1/
-wget -N https://www.raspberrypi.org/magpi-issues/Essentials_C_v1.pdf -P "$OUTDIR"
-
-# Camera Essentials - The Camera Module Guide https://www.raspberrypi.org/magpi/issues/essentials_camera_v1/
-wget -N https://www.raspberrypi.org/magpi-issues/Essentials_Camera_v1.pdf -P "$OUTDIR"
-
-# AIY VOICE ESSENTIALS - https://www.raspberrypi.org/magpi/issues/essentials-aiy-v1/
-wget -N https://www.raspberrypi.org/magpi-issues/Essentials_AIY_Projects_Voice_v1.pdf -P "$OUTDIR"
-
-# THE OFFICIAL RASPBERRY PI BEGINNER’S BOOK (2018) - https://www.raspberrypi.org/magpi/issues/beginners-1/
-wget -N https://www.raspberrypi.org/magpi-issues/Beginners_Book_v1.pdf -P "$OUTDIR"
-
-# Raspberry Pi THE ANNUAL 2018
-wget -N https://www.raspberrypi.org/magpi-issues/Annual2018.pdf -P "$OUTDIR"
-
-
+exit 0
