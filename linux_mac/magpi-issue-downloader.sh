f3d586a1afcf673309b9e80b040a19eeedbf2deb
joergi <1439809+joergi@users.noreply.github.com>
2022-10-14 22:20:59 +0200

    use 0.4.1, this fixes the problem for mac-os
    #79

--- linux_mac/magpi-issue-downloader.sh
+++ linux_mac/magpi-issue-downloader.sh
@@ -27,7 +27,7 @@ recentIssue=$(cat "$file");
 # workaround for a known limitation in bash 3.x: http://lists.gnu.org/archive/html/bug-bash/2006-01/msg00018.html
 # stackoverflow: https://stackoverflow.com/questions/32596123/why-source-command-doesnt-work-with-process-substitution-in-bash-3-2/32596626#32596626
 # shellcheck disable=SC1091
-source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/joergi/downloader/0.4.0/linux_mac/downloader.sh)" "$downloadUrl" "$OUTDIR" "$recentIssue" "$@"
+source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/joergi/downloader/0.4.1/linux_mac/downloader.sh)" "$downloadUrl" "$OUTDIR" "$recentIssue" "$@"
 
 
 exit 0
