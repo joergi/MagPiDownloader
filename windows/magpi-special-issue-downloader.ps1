180ae7bb84ced680be23e24d53003d242caccd0c
joergi <1439809+joergi@users.noreply.github.com>
2023-04-10 12:49:14 +0200

    curl remote file
    #100

--- windows/magpi-special-issue-downloader.ps1
+++ windows/magpi-special-issue-downloader.ps1
@@ -9,46 +9,28 @@
 # VERSION=0.1.0
 # USAGE="Usage: ./magpi-special-issue-downloader.ps1"
 
+# ------------------------------------------------------------------
+# [Author] joergi - https://github.com/joergi/MagPiDownloader
+#          downloader for all MagPi issues
+#          they are downloadable for free under https://www.raspberrypi.org/magpi/issues/
+#          or you can buy the paper issues under: http://swag.raspberrypi.org/collections/magpi
+#          this script is under GNU GENERAL PUBLIC LICENSE
+# ------------------------------------------------------------------
 
-# Check if directory dont exist and try create
-if ( -Not (Test-Path -Path "special_issues" ) ) {
-    New-Item -ItemType directory -Path "special_issues"
+# VERSION=0.1.0
+# USAGE="Usage: pwshl magpi-special-issue-downloader.ps1"
+
+$BASEDIR = Split-Path $MyInvocation.MyCommand.Path -Parent
+$OUTDIR = Join-Path $BASEDIR "special_issues"
+
+if (!(Test-Path $OUTDIR -PathType Container)) {
+    New-Item -ItemType Directory -Path $OUTDIR | Out-Null
 }
 
-$special_issues = @(
-    "Essentials_Games_v1.pdf",
-    "Projects_Book_v1.pdf",
-    "Projects_Book_v2.pdf",
-    "Projects_Book_v3.pdf",
-    "Essentials_Bash_v1.pdf",
-    "Essentials_SenseHAT_v1.pdf",
-    "Essentials_Sonic_Pi-v1.pdf",
-    "Essentials_Scratch_v1.pdf",
-    "Essentials_Minecraft_v1.pdf",
-    "Essentials_GPIOZero_v1.pdf",
-    "Essentials_C_v1.pdf",
-    "Essentials_Camera_v1.pdf",
-    "Essentials_AIY_Projects_Voice_v1.pdf",
-    "Beginners_Book_v1.pdf",
-    "Annual2018.pdf"
-)
+$file = Join-Path $BASEDIR "../sources-for-download/special-issues.txt"
+Get-Content $file | ForEach-Object {
+    $downloaderScript = Invoke-WebRequest https://raw.githubusercontent.com/joergi/downloader/main/windows/downloader.ps1 -UseBasicParsing
+    $downloaderScriptContent = $downloaderScript.Content
+    & pwsh -Command "& {$downloaderScriptContent  } -downloadUrl  $_ -outputDir  $OUTDIR"
+Exit 0
 
-$baseUrl = "https://www.raspberrypi.org/magpi-issues"
-$baseDir = (Split-Path -Path $PSScriptRoot -Parent)
-$web = New-Object system.net.webclient
-$errorCount = 0
-foreach ($issue in $special_issues) {
-    try {
-        Write-Verbose -Message "Downloading $issue"
-        $fileUrl = $baseUrl + "/" + $issue
-        $web.DownloadFile($fileUrl, "$baseDir\special_issues\$issue")
-    }
-    Catch {
-        Write-Verbose -Message "Ocorred an error trying download $file"
-        $errorCount++
-    }
-}
-
-if ($errorCount -gt 0) {
-    exit 1
-}
