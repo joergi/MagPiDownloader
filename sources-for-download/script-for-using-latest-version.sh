a26e6fcdc70f6ac5118ecd35fb8b6a35c55503df
joergi <1439809+joergi@users.noreply.github.com>
2023-04-19 15:08:18 +0200

    only take the newest issue

--- sources-for-download/script-for-using-latest-version.sh
+++ sources-for-download/script-for-using-latest-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+set -euo pipefail
+IFS=$'\n\t'
 # ------------------------------------------------------------------
 # based on the script Kenichi Nagai https://github.com/keni-n/
 # https://github.com/joergi/MagPiDownloader/blob/main/linux_mac/magpi-issue-downloader-latest.sh
@@ -15,8 +16,8 @@ BASEDIR=$(dirname "$0")/..
 #printf -v page_url "$MAGPI_URL/issues"
 page_url=$(printf '%s\n' "$MAGPI_URL/issues")
 
-latest_issues=$(curl -sf "$page_url" | grep "Download PDF" | sed 's/^.*issues\///' | sed 's/\/pdf.*$//')
-echo "Latest Issues is " "$latest_issues"
+latest_issue=$(curl -sf "$page_url" | grep "Download PDF" | head -n 1 | sed 's/^.*issues\///' | sed 's/\/pdf.*$//')
+echo "Latest Issue is " "$latest_issue"
 
 file="$BASEDIR/sources-for-download/regular-issues.txt";
-echo "$latest_issues" >"$file"
+echo "$latest_issue" >"$file"
