42651620eb7c6f89ad989529cc14d98e24c9079a
joergi <1439809+joergi@users.noreply.github.com>
2023-10-14 00:52:10 +0200

    downloads all issues
    - working example to download all files without -f or -l
    
    #89
    closes #89

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
-
+#!/bin/bash
+echo "hello"
 # Create the 'issues' directory with non-root ownership
-mkdir -p /app/issues
-chown myuser:myuser /app/issues
-
+#mkdir -p /app/issues
+#chown myuser:myuser /app/issues
+#chown -R myuser:myuser /app/linux_mac/magpi-issue-downloader.sh
 # List the contents of /app and /app/linux_mac for debugging
-ls -lah /app
+
 ls -lah /app/linux_mac
 
 # Execute the main command
-exec /app/linux_mac/magpi-issue-downloader.sh "$@"
+echo "pwd"
+pwd
+ls -lah
+echo "\n\n /app"
+ls -lah /app
+
+echo "\n\n /app/linux_mac"
+ls -lah /app/linux_mac
+echo "hello in entry point"
+bash /app/linux_mac/magpi-issue-downloader.sh "$@"
