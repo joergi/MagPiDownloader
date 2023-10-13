cf8bfce1af5a163a990756d26790dd0ea00ad03e
joergi <1439809+joergi@users.noreply.github.com>
2023-10-14 01:01:01 +0200

    removes all not needed debug output
    #89
    closes #89

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-echo "hello"
-# Create the 'issues' directory with non-root ownership
-#mkdir -p /app/issues
-#chown myuser:myuser /app/issues
-#chown -R myuser:myuser /app/linux_mac/magpi-issue-downloader.sh
-# List the contents of /app and /app/linux_mac for debugging
 
-ls -lah /app/linux_mac
-
-# Execute the main command
-echo "pwd"
-pwd
-ls -lah
-echo "\n\n /app"
-ls -lah /app
-
-echo "\n\n /app/linux_mac"
-ls -lah /app/linux_mac
-echo "hello in entry point"
 bash /app/linux_mac/magpi-issue-downloader.sh "$@"
